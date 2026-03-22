afaa33dd2442d8408395dc19826698d258cc9912
Kincho <kincho@kincho.org>
2026-03-22 14:34:22 +0000

    improved design

--- create_db.py
+++ create_db.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect("wallets.db")
 cursor = conn.cursor()
 
 for table in ["BCH", "BTC", "EVM", "LTC", "SOL"]:
-    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} (time TEXT, address TEXT, private_key TEXT)")
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} (time TEXT NOT NULL, address TEXT NOT NULL, private_key TEXT NOT NULL)")
 
 conn.commit()
 conn.close()
@@ -13,8 +13,8 @@ conn.close()
 conn2 = sqlite3.connect("orders.db")
 cursor2 = conn2.cursor()
 
-cursor2.execute(f"CREATE TABLE IF NOT EXISTS admin (time TEXT, order_id TEXT, LTC TEXT, BTC TEXT, BCH TEXT, EVM TEXT, SOL TEXT)")
-cursor2.execute(f"CREATE TABLE IF NOT EXISTS client (time TEXT, order_id TEXT, LTC TEXT, BTC TEXT, BCH TEXT, EVM TEXT, SOL TEXT)")
+cursor2.execute(f"CREATE TABLE IF NOT EXISTS admin (time TEXT NOT NULL, order_id TEXT NOT NULL, main TEXT NOT NULL, LTC TEXT, BTC TEXT, BCH TEXT, EVM TEXT, SOL TEXT)")
+cursor2.execute(f"CREATE TABLE IF NOT EXISTS client (time TEXT NOT NULL, order_id TEXT NOT NULL, LTC TEXT, BTC TEXT, BCH TEXT, EVM TEXT, SOL TEXT)")
 
 conn2.commit()
 conn2.close()
